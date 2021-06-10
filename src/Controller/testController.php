b1a2ba9621fde60a768e2cd283ee2e344fdab915
Michael <michaelbecquer@yahoo.fr>
2021-06-10 17:26:10 +0200

    feat: login and logout

--- src/Controller/testController.php
+++ src/Controller/testController.php
@@ -3,17 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class testController extends AbstractController
 {
 
+    /**
+     * @Route("/test/register", name="test_register_index")
+     */
+    public function index(UserPasswordEncoderInterface $password, EntityManagerInterface $manager)
+    {
 
-    // public function index(){
-
-    //     $user = new User();
-    //     $user->setEmail('admin@admin.fr')
-    //          ->setRoles(['ROLE_ADMIN'])
-
-    // }
+        $user = new User();
+        $user->setEmail('admin@admin.fr')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($password->encodePassword($user, 'admin'));
+        $manager->persist($user);
+        $manager->flush();
+        return $this->redirectToRoute("index");
+    }
 }
