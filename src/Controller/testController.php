1b8ed716629fc95f833ca729b6a35f00dd67f4cd
Michael <michaelbecquer@yahoo.fr>
2021-06-15 19:47:52 +0200

    adjust design and manage admin rights

--- src/Controller/testController.php
+++ src/Controller/testController.php
@@ -10,17 +10,21 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class testController extends AbstractController
 {
-
+    private $password;
+    public function __construct(UserPasswordEncoderInterface $password)
+    {
+        $this->password = $password;
+    }
     /**
      * @Route("/test/register", name="test_register_index")
      */
-    public function index(UserPasswordEncoderInterface $password, EntityManagerInterface $manager)
+    public function index(EntityManagerInterface $manager)
     {
 
         $user = new User();
         $user->setEmail('admin@admin.fr')
             ->setRoles(['ROLE_ADMIN'])
-            ->setPassword($password->encodePassword($user, 'admin'));
+            ->setPassword($this->password->encodePassword($user, 'admin'));
         $manager->persist($user);
         $manager->flush();
         return $this->redirectToRoute("index");
