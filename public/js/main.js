b28c41269effc4f5603527ae43f81bbde378fb3e
Michael <michaelbecquer@yahoo.fr>
2021-06-29 11:11:37 +0200

    feat:carousel product page

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
-	const menuBtn = document.querySelector('.menu-btn');
-			let menuOpen = false;
-			menuBtn.addEventListener('click', () => {
-			    if (!menuOpen) {
-			        menuBtn.classList.add('open');
-                    menuOpen = true;
-                    console.log("open");
-			
-			    } else {
-			        menuBtn.classList.remove('open');
-                    menuOpen = false;
-                    console.log('close');
-			    }
-			})
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+    console.log('open');
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    console.log('close');
+  }
+});
+$('.carousel').carousel();
