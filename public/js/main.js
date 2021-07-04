e7566639858c4eb9eaa4362797e1e0ec883a4360
Michael <michaelbecquer@yahoo.fr>
2021-07-04 22:04:43 +0200

    responsive base and index

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,19 @@
 const menuBtn = document.querySelector('.menu-btn');
+
+$(window).scroll(function () {
+  $('.arrow').css('display', 'inline');
+});
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
-    console.log('open');
+    // console.log('open');
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
-    console.log('close');
+    // console.log('close');
   }
 });
 $('.carousel').carousel();
