2cc70f9ebb9a3bbd32d3a58115153a37470d0c6e
Michael <michaelbecquer@yahoo.fr>
2021-11-23 22:56:54 +0100

    refonte css part 1 index & product page

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,38 @@
-const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.getElementById("nav-menu");
+const navClose = document.getElementById("nav-close");
+const navToggle = document.getElementById("nav-toggle");
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show-menu");
+  });
+}
 
-// $(window).scroll(function () {
-//   $('.arrow').css('display', 'inline');
-// });
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav-link");
 
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-    // console.log('open');
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+/* -------------------------------------------------------------------------- */
+/*                                  scrolltop                                 */
+/* -------------------------------------------------------------------------- */
+window.onscroll = () => {
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
   } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-    // console.log('close');
+    document.querySelector("#scroll-top").classList.remove("active");
   }
-});
+};
